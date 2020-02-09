73535fdfd059a3ed9291ba916df546562484a69a
frozmanX <56948076+frozmanX@users.noreply.github.com>
2020-02-09 14:31:15 +0300

    map

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,10 +4,13 @@ const RESOURCES = {
   "/assets\AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "/assets\FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "/assets\fonts\MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/assets\LICENSE": "f55781a1b7a28c9f46c93f682c13d830",
+"/assets\LICENSE": "0d2877f26cc90f3d63893e53c93f1e41",
 "/assets\packages\cupertino_icons\assets\CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"/index.html": "58ec0709da99eb1bdf9b988b7de97494",
-"/main.dart.js": "ad19babf508fe5bc52890e8200fde1d7"
+"/icons\Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"/icons\Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"/index.html": "db6c8cc8ade2f2121b081e2cfe78ce61",
+"/main.dart.js": "ca7785d6d73c04be54566050419f1cb8",
+"/manifest.json": "2dad83b7272518274a679e1f9d4fcca2"
 };
 
 self.addEventListener('activate', function (event) {
